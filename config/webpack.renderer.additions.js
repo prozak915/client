ac3b8f99caf994f4b43cf83cd6fdc5e96c8da3d5
Matt Huggins <matt.huggins@gmail.com>
2018-06-06 17:51:13 -0500

    WIP webpack.renderer.additions.js

--- config/webpack.renderer.additions.js
+++ config/webpack.renderer.additions.js
@@ -1,22 +1,68 @@
-const path = require('path');
+// const path = require('path');
+// const autoprefixer = require('autoprefixer');
+// const postCssFlexBugsFixes = require('postcss-flexbugs-fixes');
+const webpackRenderer = require('electron-webpack/webpack.renderer.config.js');
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        loader: 'sass-loader',
-        options: {
-          data: '@import "common/stylesheets/variables"; @import "common/stylesheets/mixins";',
-          includePaths: [path.resolve(__dirname, '..', './src')]
-          // sourceMap: isDev
-        }
-      },
-      {
-        test: /\.svg$/,
-        exclude: /node_modules/,
-        loader: 'svg-react'
-      }
-    ]
-  }
+// const isProd = process.env.NODE_ENV === 'production';
+
+function replaceSassLoader(config) {
+  return config;
+
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.(scss|sass)$/,
+  //       exclude: /node_modules/,
+  //       use: [
+  //         'style-loader',
+  //         {
+  //           loader: 'css-loader',
+  //           options: {
+  //             modules: true,
+  //             sourceMap: !isProd,
+  //             minimize: isProd,
+  //             importLoaders: 1,
+  //             localIdentName: '[name]__[local]___[hash:base64:5]'
+  //           }
+  //         },
+  //         {
+  //           loader: 'postcss-loader',
+  //           options: {
+  //             sourceMap: !isProd,
+  //             ident: 'postcss',
+  //             plugins: () => [
+  //               postCssFlexBugsFixes,
+  //               autoprefixer({
+  //                 browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9'],
+  //                 flexbox: 'no-2009'
+  //               })
+  //             ]
+  //           }
+  //         },
+  //         {
+  //           loader: 'sass-loader',
+  //           options: {
+  //             data: '
+  //               @import "common/stylesheets/variables";
+  //               @import "common/stylesheets/mixins";
+  //             ',
+  //             includePaths: [path.resolve(__dirname, '..', './src')],
+  //             sourceMap: !isProd
+  //           }
+  //         }
+  //       ]
+  //     // },
+  //     // {
+  //     //   test: /\.svg$/,
+  //     //   exclude: /node_modules/,
+  //     //   loader: 'svg-react'
+  //     }
+  //   ]
+  // };
+}
+
+module.exports = async (env) => {
+  console.log('MRH! IN FUNC', env);
+  const config = await webpackRenderer(env);
+  return replaceSassLoader(config);
 };
