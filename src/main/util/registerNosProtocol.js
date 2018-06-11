a83984d42ec6b2f8acddaa33fc93040e6ca6e85c
Matt Huggins <matt.huggins@gmail.com>
2018-06-11 18:44:43 -0500

    fixup! registerNosProtocol

--- src/main/util/registerNosProtocol.js
+++ src/main/util/registerNosProtocol.js
@@ -1,15 +1,45 @@
 import url from 'url';
+import path from 'path';
+import mime from 'mime-types';
+import fetch from 'node-fetch';
 import { protocol } from 'electron';
+import { createReadStream } from 'fs';
+import { omit } from 'lodash';
 
 import resolve from './resolve';
 
+function streamFile(uri, callback) {
+  const contentType = mime.contentType(path.extname(uri.path));
+
+  callback({
+    statusCode: 200,
+    headers: { 'content-type': contentType },
+    data: createReadStream(uri.path)
+  });
+}
+
+async function streamFetch(uri, callback) {
+  const response = await fetch(uri.href);
+
+  // Since the body is being streamed without encoding, we need to strip this header.
+  const validHeaders = omit(response.headers.raw(), 'content-encoding');
+
+  callback({
+    statusCode: response.status,
+    headers: validHeaders,
+    data: response.body
+  });
+}
+
 export default function registerNosProtocol() {
-  protocol.registerHttpProtocol('nos', async (request, callback) => {
+  protocol.registerStreamProtocol('nos', async (req, callback) => {
     try {
-      const resolvedUrl = await resolve(url.parse(request.url));
-      const result = Object.assign({}, request, { url: resolvedUrl });
-      callback(result);
+      const uri = url.parse(await resolve(url.parse(req.url)));
+      const stream = uri.protocol === 'file:' ? streamFile : streamFetch;
+
+      await stream(uri, callback);
     } catch (error) {
+      // console.error(error);
       callback({ error });
     }
   });
